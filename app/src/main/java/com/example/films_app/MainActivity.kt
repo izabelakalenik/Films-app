4b864d4311fe8906c4e007bcef0fa80d95c9c653
izabela <iza.kalenik@gmail.com>
2023-11-20 23:28:10 +0100

    descriptions and tabs added

--- app/src/main/java/com/example/films_app/MainActivity.kt
+++ app/src/main/java/com/example/films_app/MainActivity.kt
@@ -6,19 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,7 +42,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.films_app.ui.theme.Films_appTheme
-
+import androidx.compose.runtime.mutableStateOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -107,7 +116,9 @@ fun MovieItem(navController: NavController, movie: Movie) {
             .fillMaxWidth()
             .padding(16.dp)
             .clickable {
-                navController.navigate("movieDetail/${movie.title}")
+                navController.navigate("movieDetail/${movie.title}") {
+                    launchSingleTop = true
+                }
             }
     ) {
         Image(
@@ -115,7 +126,7 @@ fun MovieItem(navController: NavController, movie: Movie) {
             contentDescription = null,
             modifier = Modifier
                 .size(120.dp, 180.dp)
-                .clip(MaterialTheme.shapes.medium)
+                .clip(shape = MaterialTheme.shapes.medium)
                 .aspectRatio(4 / 6f)
                 .background(MaterialTheme.colorScheme.background)
         )
@@ -129,28 +140,139 @@ fun MovieItem(navController: NavController, movie: Movie) {
 }
 
 
-data class Movie(val title: String, val imageRes: Int)
+
+
+
+
+data class Movie(val title: String, val description: String, val imageRes: Int)
 
 val movies = listOf(
-    Movie("Captain America: The Winter Soldier", R.drawable.winter_soldier),
-    Movie("Captain America: Civil War", R.drawable.civil_war),
-    Movie("Dr Strange in the Multiverse of Madness", R.drawable.dr_strange)
+    Movie(
+        "Captain America: The Winter Soldier",
+        "Description for Captain America: The Winter Soldier goes here...",
+        R.drawable.winter_soldier
+    ),
+    Movie(
+        "Captain America: Civil War",
+        "Description for Captain America: Civil War goes here...",
+        R.drawable.civil_war
+    ),
+    Movie(
+        "Dr Strange in the Multiverse of Madness",
+        "Description for Dr Strange in the Multiverse of Madness goes here...",
+        R.drawable.dr_strange
+    )
 )
 
 @Composable
 fun MovieDetail(movie: Movie) {
-    Row(
+    val selectedTabIndex = remember { mutableStateOf(0) }
+
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(8.dp)
-    ){
-        Text(
-            text = "Opis...",
-            modifier = Modifier.weight(1f),
-            textAlign = TextAlign.Start
+    ) {
+        Image(
+            painter = painterResource(id = movie.imageRes),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .background(MaterialTheme.colorScheme.background)
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = movie.title,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Start)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Display personalized description for each movie
+        Text(
+            text = movie.description, // Add a 'description' property to your Movie data class
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Start)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // TabRow with two tabs: Scenes and Cast
+        TabRow(
+            selectedTabIndex = selectedTabIndex.value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+                .height(56.dp) // Adjust the height as needed
+        ) {
+            Tab(
+                selected = selectedTabIndex.value == 0,
+                onClick = { selectedTabIndex.value = 0 }
+            ) {
+                Text(text = "Scenes")
+            }
+            Tab(
+                selected = selectedTabIndex.value == 1,
+                onClick = { selectedTabIndex.value = 1 }
+            ) {
+                Text(text = "Cast")
+            }
+        }
+
+        // Display tab content based on the selected tab index
+        when (selectedTabIndex.value) {
+            0 -> FilmScenesTab()
+            1 -> CastTab()
+        }
     }
 }
+
+
+
+@Composable
+fun FilmScenesTab() {
+    // Content for the "Scenes" tab
+    LazyRow {
+        items(scenes) { scene ->
+            // Display scene information
+            Text(text = "Scene: ${scene.sceneName}")
+        }
+    }
+}
+
+@Composable
+fun CastTab() {
+    // Content for the "Cast" tab
+    LazyColumn {
+        items(cast) { castMember ->
+            // Display cast member information
+            Text(text = "Cast: ${castMember.actorName}")
+        }
+    }
+}
+
+data class Scene(val sceneName: String)
+data class CastMember(val actorName: String)
+
+val scenes = listOf(
+    Scene("Scene 1"),
+    Scene("Scene 2"),
+    // Add more scenes as needed
+)
+
+val cast = listOf(
+    CastMember("Actor 1"),
+    CastMember("Actor 2"),
+    // Add more cast members as needed
+)
+
+
+
+
 //@Preview(showBackground = true)
 //@Composable
 //fun DefaultPreview() {
