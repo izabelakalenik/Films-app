3705703f63ab210ebd692945c4dd974db4557c4b
izabela <iza.kalenik@gmail.com>
2023-11-20 21:06:01 +0100

    first app view

--- app/src/main/java/com/example/films_app/MainActivity.kt
+++ app/src/main/java/com/example/films_app/MainActivity.kt
@@ -3,44 +3,95 @@ package com.example.films_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.films_app.ui.theme.Films_appTheme
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Films_appTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MovieList()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit) {
+    MaterialTheme(
+        typography = Typography()
+    ) {
+        content()
+    }
 }
+
+@Composable
+fun MovieList() {
+    LazyColumn {
+        items(movies) { movie ->
+            MovieItem(movie = movie)
+        }
+    }
+}
+
+@Composable
+fun MovieItem(movie: Movie) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable { /* Handle click on movie item */ }
+    ) {
+        Image(
+            painter = painterResource(id = movie.imageRes),
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = movie.title,
+            fontWeight = FontWeight.Bold,
+
+        )
+    }
+}
+
+data class Movie(val title: String, val imageRes: Int)
+
+    val movies = listOf(
+        Movie("Movie 1", R.drawable.winter_soldier),
+        Movie("Movie 2", R.drawable.civil_war),
+        Movie("Movie 3", R.drawable.dr_strange)
+    )
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Films_appTheme {
-        Greeting("Android")
+fun DefaultPreview() {
+    MyApp {
+        MovieList()
     }
 }
