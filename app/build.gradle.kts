f3d514306d50ae9bbe4bab2127ea89bf42b8f482
izabela <iza.kalenik@gmail.com>
2023-12-02 22:08:32 +0100

    code refactoring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,14 +64,9 @@ dependencies {
     implementation ("androidx.compose.foundation:foundation:1.5.4")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation ("androidx.navigation:navigation-compose:2.7.5")
-
     implementation ("androidx.media3:media3-exoplayer:1.2.0")
     implementation ("androidx.media3:media3-ui:1.2.0")
     implementation ("androidx.media3:media3-common:1.2.0")
-
-
-
-
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
